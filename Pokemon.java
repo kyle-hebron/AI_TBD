e9fac12ef337c36522366199851f852e0e7d41bb
Smooth Toast <thewaffleminister@gmail.com>
2022-06-14 18:17:28 -0700

    Added Pokemon Class

---
+++ Pokemon.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Pokemon{
+
+    String name;
+    int attack, defense, speed;
+
+    Pokemon(String name, int attack, int defense, int speed) {
+        this.name = name;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+    }
+
+
+}
