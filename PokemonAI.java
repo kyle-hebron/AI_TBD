721cfb6512e06724c3225bae79a4809ddffee4da
Smooth Toast <thewaffleminister@gmail.com>
2022-06-14 22:46:22 -0700

    Added intro

--- PokemonAI.java
+++ PokemonAI.java
@@ -5,20 +5,31 @@ public class PokemonAI{
     
     
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
 
         Scanner scan = new Scanner(System.in);
         String name, rival;
+
         System.out.println("Hello there! Welcome to the world of POKEMON! My name is OAK! People call me the POKEMON PROF!");
+        Thread.sleep(2000);
         System.out.println("This world is inhabited by creatures called POKEMON!");
+        Thread.sleep(2000);
         System.out.println("For some people, POKEMON are pets. Others use them for fights. Myself...I study POKEMON as a profession.");
+        Thread.sleep(2000);
         System.out.println("First, what is your name?");
         name = scan.nextLine();
         System.out.println("Right! So your name is " + name + "!");
+        Thread.sleep(2000);
         System.out.println("This is my grandson. He's been your rival since you were a baby. ...Erm, what is his name again?");
         rival = scan.nextLine();
-        
-
+        Thread.sleep(2000);
+        System.out.println("That's right! I remember now! His name was " + rival + "! ");
+        Thread.sleep(2000);
+        System.out.println( name + "!" + " Your very own POKEMON legend is about to unfold!");
+        Thread.sleep(2000);
+        System.out.println("A world of dreams and adventures with POKEMON awaits! Let's go!");
+        scan.close();
         PokemonTeam user = new PokemonTeam(name);
+        PokemonTeam enemy1 = new PokemonTeam(rival);
     }
 }
