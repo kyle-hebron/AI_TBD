d2059316f170cd6fdc594267c0740d55d7fbf209
mattdevera <85967593+mattdevera@users.noreply.github.com>
2022-06-15 22:15:55 -0700

    Updated Pokemon.java
    added getter/setter methods for the stats, added HP

--- Pokemon.java
+++ Pokemon.java
@@ -3,14 +3,35 @@ import java.util.*;
 public class Pokemon{
 
     String name;
-    int attack, defense, speed;
+    int health, attack, defense, speed;
 
-    Pokemon(String name, int attack, int defense, int speed) {
+    Pokemon(String name, int health, int attack, int defense, int speed) {
         this.name = name;
+        this.health = health;
         this.attack = attack;
         this.defense = defense;
         this.speed = speed;
     }
-
-
+    
+    //setter method only really needed for health, other stats should remain the same (for now)
+    private void setHealth(int newHP) {
+        health = newHP;
+    }
+    //getter methods
+    private String getName() {
+        return name;
+    }
+    private String getHP() {
+        return health;
+    }
+    private String getAtk() {
+        return attack;
+    }
+    private String getDef() {
+        return defense;
+    }
+    private String getSpd() {
+        return speed;
+    }
+    
 }
