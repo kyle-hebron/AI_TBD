72abde4744ea179c7de60a75af27dd18c4a784eb
Smooth Toast <thewaffleminister@gmail.com>
2022-06-15 22:37:41 -0700

    Added type

--- Pokemon.java
+++ Pokemon.java
@@ -3,13 +3,14 @@ import java.util.*;
 public class Pokemon{
 
     String name;
-    int attack, defense, speed;
+    int attack, defense, speed, type;
 
-    Pokemon(String name, int attack, int defense, int speed) {
+    Pokemon(String name, int attack, int defense, int speed, int type) {
         this.name = name;
         this.attack = attack;
         this.defense = defense;
         this.speed = speed;
+        this.type = type;
     }
 
 
--- PokemonAI.java
+++ PokemonAI.java
@@ -2,7 +2,15 @@ import java.util.*;
 
 
 public class PokemonAI{
-    
+
+    int[][] type = {
+        {2, 2, 1, 1, 0, 0},     //0 = water, 1 = grass, 2 = fire, 3 = ground, 4 = electric, 5 = flying
+        {1, 2, 2, 1, 0, 2},     //0 means neutral, 1 means super effective, 2 means not very effective, 3 means no effect
+        {2, 1, 2, 0, 0, 0},     //type[attack][defend]
+        {0, 2, 1, 0, 1, 3}, 
+        {1, 0, 0, 3, 0, 1}, 
+        {0, 1, 0, 0, 2, 0}
+    };
     
 
     public static void main(String[] args) throws InterruptedException{
