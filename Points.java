a3ddc61f2406a5382392aa33ee0d16827f9ea4cc
mdsou <mardit.dersoukiassian.409@my.csun.edu>
2022-06-22 16:32:26 -0700

    Update Points.java
    Jesus.

--- Points.java
+++ Points.java
@@ -1,27 +1,16 @@
-import java.util.*;
-import java.util.ArrayList;
+import java.util.*; 
 
 public class Points{
-
-    ArrayList<Integer> ptsArr;
+ 
     int totalPts; 
 
     public Points(int totalPts){ 
     
-        this.totalPts = totalPts;
-        ptsArr = new ArrayList<Integer>();
-    }  
+        this.totalPts = totalPts; 
+    }   
 
-    public void setPoints(Moves damage){   
-
-        ptsArr.add(damage.getDamage()); 
-
-        totalPts = 0;
-
-        for(int i = 0; i < ptsArr.size(); i++){
-            totalPts += ptsArr.get(i);
-        }
-            
+    public void addDamage(Moves damage){ 
+        totalPts += damage.getDamage();
     }
 
     public int getPoints(){ 
