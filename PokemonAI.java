41dad364d41bf3bef6bf368540b353c6e8dfe7ff
Smooth Toast <thewaffleminister@gmail.com>
2022-06-24 17:55:31 -0700

    Added comments

--- PokemonAI.java
+++ PokemonAI.java
@@ -81,7 +81,7 @@ public class PokemonAI{
         Battle currentBattle = new Battle(player, Leader1);
 
         currentBattle.chooseOption();
-        
+        //while 
                 
         //Ignore this
                 
