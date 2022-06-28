45b3076d09cb21ed14b432dc06b69120f11c9b3b
mdsou <mardit.dersoukiassian.409@my.csun.edu>
2022-06-27 22:52:39 -0700

    skeleton of the graph

---
+++ Temp.java
@@ -0,0 +1,22 @@
+// public class Temp {
+    
+ 
+
+//     //calculate heuristic value
+      
+//     if(heuristic_val > enemy.heuristicVal){ 
+//         switch; 
+//     }else if(heuristic_val < enemy.heuristicVal){
+//         return;
+//     }else{
+//         if(health < 25%){
+//             switch;
+//         }
+//         if(health > 75%){
+//             return;
+//         }
+//     }
+
+    
+
+// }
