cdfead294374f85c365d2971271fc527e2096075
mattdevera <matthew.devera.351@my.csun.edu>
2022-07-08 17:35:56 -0700

    Added documentation to minimax.java.

--- minimax.java
+++ minimax.java
@@ -2,22 +2,19 @@
 // Java program to demonstrate
 // working of Alpha-Beta Pruning
 
-class Minimax { //changed class name for our purposes
+class Minimax { //Changed class name for our purposes.
 
-    // Initial values of
-    // Alpha and Beta
+    // Initial values of Alpha and Beta.
         static int MAX = Integer.MAX_VALUE;
         static int MIN = Integer.MIN_VALUE;
     
-    // Returns optimal value for
-    // current player (Initially called
-    // for root and maximizer)
+    // Returns optimal value for current player.
+    // Initially called for root and maximizer.
         static int minimax(int depth, int nodeIndex,
                 Boolean maximizingPlayer,
                 int values[], int alpha,
                 int beta) {
-            // Terminating condition. i.e
-            // leaf node is reached
+            // Terminating condition. i.e. Leaf node is reached.
             if (depth == 3) {
                 return values[nodeIndex];
             }
@@ -26,8 +23,7 @@ class Minimax { //changed class name for our purposes
     
                 int best = MIN;
     
-                // Recur for left and
-                // right children
+                // Recursion for the left and right children.
                 for (int i = 0; i < 2; i++) {
     
                     int val = minimax(depth + 1, nodeIndex * 2 + i,
@@ -35,7 +31,7 @@ class Minimax { //changed class name for our purposes
                     best = Math.max(best, val);
                     alpha = Math.max(alpha, best);
     
-                    // Alpha Beta Pruning
+                    // Alpha-Beta pruning.
                     if (beta <= alpha) {
                         break;
                     }
@@ -44,8 +40,7 @@ class Minimax { //changed class name for our purposes
             } else {
                 int best = MAX;
     
-                // Recur for left and
-                // right children
+                // Recursion for the left and right children.
                 for (int i = 0; i < 2; i++) {
     
                     int val = minimax(depth + 1, nodeIndex * 2 + i,
@@ -53,7 +48,7 @@ class Minimax { //changed class name for our purposes
                     best = Math.min(best, val);
                     beta = Math.min(beta, best);
     
-                    // Alpha Beta Pruning
+                    // Alpha-Beta pruning.
                     if (beta >= alpha) {
                         break;
                     }
@@ -63,13 +58,15 @@ class Minimax { //changed class name for our purposes
         }
     
     ////////////////////////////////////////////END GEEKS FOR GEEKS CODE
-        //Helper Functions
-        //determines whether the AI should attack or switch
+        //Helper functions for the mini-max algorithm to work for the Pokemon battle simulator.
+
+        //Determines whether the AI should attack or switch.
         public int aiAtkOrSwitch(PokemonTeam myTeam, Pokemon me, Pokemon target) {
-            //calculate values for each pokemon and attack
+            //Calculate heuristic values for each Pokemon and attack.
             int choice = -1;
             int[] hValues = new int[8];
     
+            //Initializes the h-values in an array, the first four values being the moves, and the last two values being the other two Pokemon on the team.
             for (int i = 0; i < 4; i++) {
                 Moves move = me.moveList[i];
                 hValues[i] = (int) aiChooseAtk(move, target, me);
@@ -81,8 +78,8 @@ class Minimax { //changed class name for our purposes
                 hValues[i] = 0;
             }
             int res = minimax(0, 0, true, hValues, MIN, MAX);
-            //find the position of the value of res within hvals[] in order to determine the action taken
-            //take that action
+            //Find the position of the value of mini-max's result within hValues[] in order to determine the action taken.
+            //Take that action.
             for (int i = 0; i < 6; i++) {
                 int location = hValues[i];
                 if (location == res) {
@@ -91,14 +88,14 @@ class Minimax { //changed class name for our purposes
                 }
             }
             return choice;
-            //position 0 - 5; 0 - 3 are moves 4 & 5 switch
+            //Position 0 - 5: 0 - 3 are moves, 4 and 5 are Pokemon to switch to.
         }
     
-        //determines a move's heuristic value in the current turn
+        //Determines a move's heuristic value in the current turn.
         public double aiChooseAtk(Moves move, Pokemon target, Pokemon user) {
             double hValue = 0.0;
 
-            PokemonTeam temps = new PokemonTeam("");    //Filler
+            PokemonTeam temps = new PokemonTeam("");
             PokemonTeam temper = new PokemonTeam("");
             temps.insertPokemon(target);
             temper.insertPokemon(user);
@@ -109,7 +106,10 @@ class Minimax { //changed class name for our purposes
             return hValue;
         }
     
-        //determines a Pokemon's heuristic value in the current turn
+        //Determines a Pokemon's heuristic value in the current turn.
+        //This is the first version of the switch heuristic function.
+        //Keep implemented (and commented) just in case the current fuction breaks as we add new features.
+        //This implementation works, just does not occur as accurately as it should.
         /*
         public double aiPokeSwitch(Pokemon toSwitch, Pokemon target){
             double hValue = 1.0;
@@ -128,14 +128,16 @@ class Minimax { //changed class name for our purposes
             return hValue; 
         }
          */
-        //needs to take in a team?
+        
+         //Current version of the switch heuristic function.
+         //Does NOT yet determine the health of the remaining Pokemon into the heuristic value calculation. (Maybe in the next patch?)
         public double aiPokeSwitch(PokemonTeam team, Pokemon me, Pokemon target) {
             double hValue = 1.0;
     
             Type myType = me.getPokeType();
             Type targetType = target.getPokeType();
     
-            //if the enemy is effective against me, i should switch
+            //Ff the enemy is effective against me, I should switch.
             if (targetType.getEffectiveness(myType) == 2.0) {
                 int i = 1;
                 while (i < 3) {
@@ -157,7 +159,7 @@ class Minimax { //changed class name for our purposes
                         }
                         break;
                     }
-                    //if the Pokemon i want to switch to is super effective against the enemy, switch to that Pokemon
+                    //If the Pokemon I want to switch to is super effective against the enemy, switch to that Pokemon.
                     if (toSwitchType.getEffectiveness(targetType) == 2.0) {
                         Moves moveList[] = toSwitch.getMoveList();
                         for (int j = 0; j < moveList.length; j++) {
